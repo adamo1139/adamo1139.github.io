9d7fbb07bb78175d09f6856d85fd4c584b441880
adamo1139 <adamo1139@gmail.com>
2019-05-10 16:39:12 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+var modal = document.querySelector(".modal");
+var button = document.querySelector(".button");
+var closebutton = document.querySelector("dialog > .button");
+
+function buttonOnClick(event) {
+    if (modal.open == true) {
+        modal.close();
+    } else if (modal.open == false) {
+        modal.showModal();
+    }
+}
+
+button.addEventListener("click", buttonOnClick);
+closebutton.addEventListener("click", buttonOnClick);
